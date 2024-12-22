cdc60265cc4c73ba689057a7fc4831798e683bbb
sumasubbu <75.suma@gmail.com>
2024-12-22 22:47:08 +0530

    partially coded the header and body

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+import React from 'react';
+
 import logo from './asset/logo.png';
 import './index.css';
 
@@ -21,22 +22,376 @@ import './index.css';
    - Contact
    - Address
  */
+
+const resList = [
+  {
+    info: {
+      id: '75491',
+      name: 'California Burrito',
+      cloudinaryImageId:
+        'RX_THUMBNAIL/IMAGES/VENDOR/2024/9/3/45f651c7-cc3b-45f1-8098-dba49a8c7089_75491.jpg',
+      locality: 'Yelahanka',
+      areaName: 'Yelahanka',
+      costForTwo: '₹250 for two',
+      cuisines: [
+        'Mexican',
+        'American',
+        'Salads',
+        'Continental',
+        'Keto',
+        'Healthy Food',
+        'Beverages',
+        'Snacks',
+        'Desserts',
+        'Fast Food',
+      ],
+      avgRating: 4.6,
+      parentId: '1252',
+      avgRatingString: '4.6',
+      totalRatingsString: '16K+',
+      sla: {
+        deliveryTime: 31,
+        lastMileTravel: 5,
+        serviceability: 'SERVICEABLE',
+        slaString: '30-35 mins',
+        lastMileTravelString: '5.0 km',
+        iconType: 'ICON_TYPE_EMPTY',
+      },
+      availability: {
+        nextCloseTime: '2024-12-22 23:00:00',
+        opened: true,
+      },
+      badges: {
+        textExtendedBadges: [
+          {
+            iconId: 'guiltfree/GF_Logo_android_3x',
+            shortDescription: 'options available',
+            fontColor: '#7E808C',
+          },
+        ],
+      },
+      isOpen: true,
+      type: 'F',
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: '',
+                  fontColor: '#7E808C',
+                  iconId: 'guiltfree/GF_Logo_android_3x',
+                  shortDescription: 'options available',
+                },
+              },
+            ],
+          },
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: '50% OFF',
+        subHeader: 'UPTO ₹100',
+      },
+      differentiatedUi: {
+        displayType: 'ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT',
+        differentiatedUiMediaDetails: {
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: 'RESTAURANT_DISPLAY_TYPE_DEFAULT',
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: '4.3',
+          ratingCount: '762',
+        },
+        source: 'GOOGLE',
+        sourceIconImageId: 'v1704440323/google_ratings/rating_google_tag',
+      },
+      ratingsDisplayPreference: 'RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY',
+    },
+    analytics: {
+      context: 'seo-data-521e3898-d6be-4adc-b533-c8f9a2aace70',
+    },
+    cta: {
+      link: 'https://www.swiggy.com/city/bangalore/california-burrito-yelahanka-rest75491',
+      type: 'WEBLINK',
+    },
+  },
+  {
+    info: {
+      id: '609594',
+      name: 'CakeZone Patisserie',
+      cloudinaryImageId:
+        'RX_THUMBNAIL/IMAGES/VENDOR/2024/12/19/3b5b0481-8a3a-44f7-b8fc-7bf8f0a624da_609594.JPG',
+      locality: 'KODIGEHALLI',
+      areaName: 'Maruthi Layout',
+      costForTwo: '₹200 for two',
+      cuisines: ['Bakery', 'Desserts', 'Sweets', 'Ice Cream'],
+      avgRating: 4.3,
+      parentId: '7003',
+      avgRatingString: '4.3',
+      totalRatingsString: '1.9K+',
+      sla: {
+        deliveryTime: 33,
+        lastMileTravel: 3.7,
+        serviceability: 'SERVICEABLE',
+        slaString: '30-35 mins',
+        lastMileTravelString: '3.7 km',
+        iconType: 'ICON_TYPE_EMPTY',
+      },
+      availability: {
+        nextCloseTime: '2024-12-23 02:00:00',
+        opened: true,
+      },
+      badges: {
+        textExtendedBadges: [
+          {
+            iconId: 'guiltfree/GF_Logo_android_3x',
+            shortDescription: 'options available',
+            fontColor: '#7E808C',
+          },
+        ],
+      },
+      isOpen: true,
+      type: 'F',
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: '',
+                  fontColor: '#7E808C',
+                  iconId: 'guiltfree/GF_Logo_android_3x',
+                  shortDescription: 'options available',
+                },
+              },
+            ],
+          },
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: '35% OFF',
+        discountTag: 'FLAT DEAL',
+      },
+      differentiatedUi: {
+        displayType: 'ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT',
+        differentiatedUiMediaDetails: {
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: 'RESTAURANT_DISPLAY_TYPE_DEFAULT',
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: '--',
+        },
+      },
+      ratingsDisplayPreference: 'RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY',
+    },
+    analytics: {
+      context: 'seo-data-521e3898-d6be-4adc-b533-c8f9a2aace70',
+    },
+    cta: {
+      link: 'https://www.swiggy.com/city/bangalore/cakezone-patisserie-kodigehalli-maruthi-layout-rest609594',
+      type: 'WEBLINK',
+    },
+  },
+  {
+    info: {
+      id: '618021',
+      name: 'Bakingo',
+      cloudinaryImageId:
+        'RX_THUMBNAIL/IMAGES/VENDOR/2024/12/18/6f9d1b2a-ce37-448f-8d77-16df0806e24a_618021.JPG',
+      locality: 'New Town Main Road',
+      areaName: 'Yelahanka',
+      costForTwo: '₹299 for two',
+      cuisines: ['Bakery', 'Desserts', 'Beverages', 'Snacks'],
+      avgRating: 4.5,
+      parentId: '3818',
+      avgRatingString: '4.5',
+      totalRatingsString: '2.7K+',
+      sla: {
+        deliveryTime: 24,
+        lastMileTravel: 4,
+        serviceability: 'SERVICEABLE',
+        slaString: '20-25 mins',
+        lastMileTravelString: '4.0 km',
+        iconType: 'ICON_TYPE_EMPTY',
+      },
+      availability: {
+        nextCloseTime: '2024-12-23 01:00:00',
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: 'F',
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: '50% OFF',
+        subHeader: 'UPTO ₹100',
+      },
+      differentiatedUi: {
+        displayType: 'ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT',
+        differentiatedUiMediaDetails: {
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: 'RESTAURANT_DISPLAY_TYPE_DEFAULT',
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: '--',
+        },
+      },
+      ratingsDisplayPreference: 'RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY',
+    },
+    analytics: {
+      context: 'seo-data-521e3898-d6be-4adc-b533-c8f9a2aace70',
+    },
+    cta: {
+      link: 'https://www.swiggy.com/city/bangalore/bakingo-new-town-main-road-yelahanka-rest618021',
+      type: 'WEBLINK',
+    },
+  },
+  {
+    info: {
+      id: '64755',
+      name: 'A2B - Adyar Ananda Bhavan',
+      cloudinaryImageId:
+        'RX_THUMBNAIL/IMAGES/VENDOR/2024/12/22/8bf25f2a-fc0d-4ab1-b73c-ef1c3a213c06_64755.jpg',
+      locality: 'HMT Layout',
+      areaName: 'Vidyaranyapura',
+      costForTwo: '₹300 for two',
+      cuisines: ['South Indian', 'North Indian', 'Sweets', 'Chinese'],
+      avgRating: 4.5,
+      veg: true,
+      parentId: '22',
+      avgRatingString: '4.5',
+      totalRatingsString: '29K+',
+      sla: {
+        deliveryTime: 24,
+        lastMileTravel: 1,
+        serviceability: 'SERVICEABLE',
+        slaString: '20-25 mins',
+        lastMileTravelString: '1.0 km',
+        iconType: 'ICON_TYPE_EMPTY',
+      },
+      availability: {
+        nextCloseTime: '2024-12-22 22:30:00',
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: 'bolt/Bolt%20Listing%20badge@3x.png',
+            description: 'bolt!',
+          },
+          {
+            imageId: 'Rxawards/_CATEGORY-South%20Indian.png',
+            description: 'Delivery!',
+          },
+        ],
+      },
+      isOpen: true,
+      type: 'F',
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: 'bolt!',
+                  imageId: 'bolt/Bolt%20Listing%20badge@3x.png',
+                },
+              },
+              {
+                attributes: {
+                  description: 'Delivery!',
+                  imageId: 'Rxawards/_CATEGORY-South%20Indian.png',
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: 'ITEMS',
+        subHeader: 'AT ₹29',
+      },
+      differentiatedUi: {
+        displayType: 'ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT',
+        differentiatedUiMediaDetails: {
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: 'RESTAURANT_DISPLAY_TYPE_DEFAULT',
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: '--',
+        },
+      },
+      ratingsDisplayPreference: 'RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY',
+    },
+    analytics: {
+      context: 'seo-data-521e3898-d6be-4adc-b533-c8f9a2aace70',
+    },
+    cta: {
+      link: 'https://www.swiggy.com/city/bangalore/a2b-adyar-ananda-bhavan-hmt-layout-vidyaranyapura-rest64755',
+      type: 'WEBLINK',
+    },
+  },
+];
+
 // TODO: change the font weight for this div elements
 
-const ResCard = () => {
+const ResCard = (props) => {
+  // const { resName, resCuisines, resDelivery,resRating  } = props;
+   const { resObj } = props;
+  console.log(resObj);
+  const { name, cuisines, avgRating, costForTwo, cloudinaryImageId } =
+    resObj?.info;
+  const { deliveryTime } = resObj?.info?.sla;
+  if (!resObj) {
+    return <h1>Loading...</h1>;
+  }
   return (
     <div className="res-card">
       <img
-        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/FOOD_CATALOG/IMAGES/CMS/2024/6/30/022946e7-5595-4dcd-b1cb-42ddd9885a25_d8ed0aed-4dad-4001-a7b5-cab01294f611.jpg"
+        src={
+          'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/' +
+          cloudinaryImageId
+        }
         alt="food-image"
       ></img>
-      <h3 className='res-title'>Ciro's Pizzeria</h3>
+      <h3 className="res-title">{name}</h3>
       <div className="res-description">
-      
-
-      <h5 >Italian, Desserts, Pizzas, Pastas</h5>
-      <h5>30 mins</h5>
-      <h5>4.4 stars</h5>
+        <h5>{cuisines.join(', ')}</h5>
+        <h5>{deliveryTime} minutes</h5>
+        <h5> {avgRating} stars</h5>
+        <h5>{costForTwo} </h5>
       </div>
     </div>
   );
@@ -47,18 +402,9 @@ const Body = () => {
     <div className="body-container">
       <div className="search">Search</div>
       <div className="res-container">
-      <ResCard/>
-      <ResCard/>
-      <ResCard/>
-      <ResCard/>
-      <ResCard/>
-      <ResCard/>
-      <ResCard/>
-      <ResCard/>
-      <ResCard/>
-      <ResCard/>
-      <ResCard/>
-      <ResCard/>
+        {resList.map((restaurant) => (
+          <ResCard key={restaurant.info.id} resObj={restaurant} />
+        ))}
       </div>
     </div>
   );
@@ -77,7 +423,6 @@ const Header = () => {
           <li>Cart</li>
         </ul>
       </div>
-      
     </div>
   );
 };
@@ -86,7 +431,7 @@ const App = () => {
   return (
     <div className="app">
       <Header />
-      <Body/>
+      <Body />
     </div>
   );
 };
--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # Food_Ordering_App
-## What is flex box? flex-wrap property? why do we use it?
+### What is flex box? flex-wrap property? why do we use it?
+### what is Optional chaining (?.) ?
+- visit https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining 
+- short form of ternary operator wherein ternary can returns 0 after evaluation, but optional chaining returns undefined instead of 0 and avoids throwing an error
+### installed JSON viewer extension in chrome
+
+### notes
+- when large scale sites are built the images are hosted in CDN and serviced from CDN to browsers
+
+## Process of developing this website
+
+### Planning
+- sketch how the UI should look like onto a paper
+- The main page should have a 
+ - Header
+  - logo
+  - nav items
+ - Body
+  - search
+  - restaurant container
+   - restaurant card
+    - image
+    - restaurant name
+    - cuisines, delivery time, star rating, cost for two
+ - Footer
+  - copyright
+  - links
+  - contact 
+  - address
+- Github repo setup  
+ - create a new repo on github without a readme
+ - create a project folder locally in the system and open in an editor
+ - in the terminal type the following commands
+  - echo "# project-name" >> README.md
+  - git init
+  - git add README.md
+  - git commit -m "first commit"
+  - git branch -M "main" (main branch is called "master" in local repo. Remote repo main branch is "main". To make them match rename the local branch from "master" to "main")
+  - git remote add origin remote-repo-url.git (this url can be found on github -> project repo-> url)
+  - git push -u origin main (push local branch "main" to remote branch "main")
+- install npm
+- install parcel as a dev dependency
+- install react, react-dom
+- add scripts to run and build the app in package.json
+- add .gitignore file to the root of the project directory
+ - add these to the file
+  - /node_modules
+  - /.vscode
+  - /dist
+  - /.parcel-cache
+ - These folders need not be pushed to github as they can be regenerated when the project is cloned
+- create App.js, index.html, index.css files in the project folder
+- in index.html, specify type = "module" in <script/> tag to import react and react-dom as modules in App.js
+- import react, react-dom from react libraries and also index.css in App.js
+- 
+
+
